c5b77aa97974bffea1141c3cd3fdec874c1b66c1
GuilleAlvareez <guillealvarezmoreno@gmail.com>
2025-01-10 13:02:46 +0100

    Dia 3  revision de proyecto y arganizando y cambio en el modelo

--- gymApp/models.py
+++ gymApp/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Musculo(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Musculo'
+        verbose_name_plural = 'Musculos'
+
+    def __str__(self):
+        return f"{self.nombre}"
+
 class Ejercicio(models.Model):
     nombre = models.CharField(max_length=100)
+    musculos = models.ManyToManyField(Musculo)
+    visibilidad = models.CharField(max_length=200, default='all')
 
     class Meta:
         verbose_name = 'Ejercicio'
@@ -11,45 +24,31 @@ class Ejercicio(models.Model):
     def __str__(self):
         return f"{self.nombre}"
 
+class Entreno(models.Model):
+    fecha = models.DateField()
+    valoracion = models.IntegerField()
+    ejercicios = models.IntegerField()
+    comentarios = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name = 'Entreno'
+        verbose_name_plural = 'Entrenos'
+
+    def __str__(self):
+        return f"{self.fecha}"
+
+
 class Ejercicio_realizado(models.Model):
-    nombre = models.ForeignKey(Ejercicio, on_delete=models.DO_NOTHING)
+    entreno = models.ForeignKey(Entreno, on_delete=models.CASCADE)
+    nombre = models.ForeignKey(Ejercicio, on_delete=models.CASCADE)
     peso = models.IntegerField()
     series = models.IntegerField()
     repeticiones = models.IntegerField()
     observaciones = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = 'Ejercicio_realizado'
+        verbose_name_plural = 'Ejercicios_realizados'
+
     def __str__(self):
         return f"{self.peso}kg {self.series}x{self.repeticiones}"
-
-class Bloque(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
-
-    class Meta:
-        verbose_name = 'Bloque'
-        verbose_name_plural = 'Bloques'
-
-    def __str__(self):
-        return f"{self.nombre}"
-    
-class Semana(models.Model):
-    bloque = models.ForeignKey(Bloque, on_delete=models.CASCADE)
-    numero = models.IntegerField()
-
-    class Meta:
-        verbose_name = 'Semana'
-        verbose_name_plural = 'Semanas'
-
-    def __str__(self):
-        return f"{self.bloque.nombre}: {self.numero}"
-    
-class Dia(models.Model):
-    semana = models.ForeignKey(Semana, on_delete=models.CASCADE)
-    nombre_dia = models.CharField(max_length=100)
-    ejercicios = models.ManyToManyField(Ejercicio)
-
-    class Meta:
-        verbose_name = 'Dia'
-        verbose_name_plural = 'Dias'
-
-    def __str__(self):
-        return f"{self.semana.numero}: {self.nombre_dia}"
